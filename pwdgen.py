769c72fa2bb31e9e310bd7015bdb90e91812e2a8
thouay-baccam <thouay.baccam@laplateforme.io>
2023-12-05 15:30:21 +0100

    MaJ vraiment final (partie du code était inutile)

--- pwdgen.py
+++ pwdgen.py
@@ -5,25 +5,21 @@ import string
 
 class GestionMotDePasse:
     def __init__(self, chemin_fichier="mots_de_passe.json"):
-        # Initialisation de la classe avec le chemin du fichier où les mots de passe sont stockés.
         self.chemin_fichier = chemin_fichier
-        self.mots_de_passe = self.charger()  # On charge les mots de passe existants depuis le fichier.
+        self.mots_de_passe = self.charger()
 
     def charger(self):
-        # Tentative de chargement du fichier JSON contenant les mots de passe.
         try:
             with open(self.chemin_fichier, "r") as fichier:
                 return json.load(fichier)
         except (FileNotFoundError, json.JSONDecodeError):
-            return {}  # Retourne un dictionnaire vide en cas d'erreur ou si le fichier est vide.
+            return {}
 
     def sauvegarder(self):
-        # Sauvegarde les mots de passe dans le fichier JSON.
         with open(self.chemin_fichier, "w") as fichier:
-            json.dump(self.mots_de_passe, fichier)
+            json.dump(self.mots_de_passe, fichier, indent=2)
 
     def verif_exigences(self, mdp):
-        # Vérifie si le mot de passe respecte les exigences de sécurité.
         return (
             len(mdp) >= 8
             and any(char.isupper() for char in mdp)
@@ -33,11 +29,9 @@ class GestionMotDePasse:
         )
 
     def encrypter(self, mdp):
-        # Crypte le mot de passe en utilisant la fonction de hachage SHA-256.
         return hashlib.sha256(mdp.encode()).hexdigest()
 
     def gen_mdp_alea(self):
-        # Génère un mot de passe aléatoire qui respecte les exigences de sécurité.
         longueur = random.randint(8, 12)
         caracteres = string.ascii_letters + string.digits + "!@#$%^&*"
         mdp = "".join(random.choice(caracteres) for _ in range(longueur))
@@ -48,24 +42,19 @@ class GestionMotDePasse:
         return mdp
 
     def ajoutmdp(self, utilisateur, mdp=None):
-        # Ajoute un nouveau mot de passe pour un utilisateur donné.
         if mdp is None:
-            mdp = self.gen_mdp_alea()  # Génère un mot de passe aléatoire si aucun n'est fourni.
+            mdp = self.gen_mdp_alea()
 
         if utilisateur in self.mots_de_passe:
-            print("Cet utilisateur a déjà un mot de passe enregistré.")
+            print(f"Un mot de passe existe déjà pour l'utilisateur {utilisateur}. Veuillez choisir un autre nom d'utilisateur.")
             return
 
-        if self.verif_exigences(mdp):
-            mdp_crypte = self.encrypter(mdp)
-            self.mots_de_passe[utilisateur] = mdp_crypte
-            self.sauvegarder()
-            print(f"Mot de passe enregistré avec succès pour {utilisateur}.")
-        else:
-            print("Le mot de passe généré ne respecte pas les exigences de sécurité. Veuillez réessayer.")
+        mdp_crypte = self.encrypter(mdp)
+        self.mots_de_passe[utilisateur] = mdp_crypte
+        self.sauvegarder()
+        print(f"Mot de passe enregistré avec succès pour {utilisateur}.")
 
     def affichermotsdepasse(self):
-        # Affiche tous les mots de passe enregistrés.
         if self.mots_de_passe:
             print("Mots de passe enregistrés :")
             for utilisateur, mdp_crypte in self.mots_de_passe.items():
