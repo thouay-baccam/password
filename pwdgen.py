e208ef367ba53281facf8c34fc84024f4c7947b2
thouay-baccam <thouay.baccam@laplateforme.io>
2023-12-09 23:55:19 +0100

    Overhaul du code

--- pwdgen.py
+++ pwdgen.py
@@ -1,89 +1,96 @@
+import re
 import hashlib
 import json
 import random
 import string
 
-class GestionMotDePasse:
-    def __init__(self, chemin_fichier="mots_de_passe.json"):
-        self.chemin_fichier = chemin_fichier
-        self.mots_de_passe = self.charger()
+# Fonction pour vérifier si le mot de passe respecte les exigences de sécurité
+def verifmdpexigence(mdp):
+    return (
+        len(mdp) >= 8 and
+        any(char.isupper() for char in mdp) and
+        any(char.islower() for char in mdp) and
+        any(char.isdigit() for char in mdp) and
+        any(char in "!@#$%^&*" for char in mdp)
+    )
 
-    def charger(self):
-        try:
-            with open(self.chemin_fichier, "r") as fichier:
-                return json.load(fichier)
-        except (FileNotFoundError, json.JSONDecodeError):
-            return {}
-
-    def sauvegarder(self):
-        with open(self.chemin_fichier, "w") as fichier:
-            json.dump(self.mots_de_passe, fichier, indent=2)
-
-    def verif_exigences(self, mdp):
-        return (
-            len(mdp) >= 8
-            and any(char.isupper() for char in mdp)
-            and any(char.islower() for char in mdp)
-            and any(char.isdigit() for char in mdp)
-            and any(char in "!@#$%^&*" for char in mdp)
-        )
-
-    def encrypter(self, mdp):
-        return hashlib.sha256(mdp.encode()).hexdigest()
-
-    def gen_mdp_alea(self):
-        longueur = random.randint(8, 12)
-        caracteres = string.ascii_letters + string.digits + "!@#$%^&*"
-        mdp = "".join(random.choice(caracteres) for _ in range(longueur))
-
-        while not self.verif_exigences(mdp):
-            mdp = "".join(random.choice(caracteres) for _ in range(longueur))
-
-        return mdp
-
-    def ajoutmdp(self, utilisateur, mdp=None):
-        if mdp is None:
-            mdp = self.gen_mdp_alea()
-
-        if utilisateur in self.mots_de_passe:
-            print(f"Un mot de passe existe déjà pour l'utilisateur {utilisateur}. Veuillez choisir un autre nom d'utilisateur.")
-            return
-
-        mdp_crypte = self.encrypter(mdp)
-        self.mots_de_passe[utilisateur] = mdp_crypte
-        self.sauvegarder()
-        print(f"Mot de passe enregistré avec succès pour {utilisateur}.")
-
-    def affichermotsdepasse(self):
-        if self.mots_de_passe:
-            print("Mots de passe enregistrés :")
-            for utilisateur, mdp_crypte in self.mots_de_passe.items():
-                print(f"{utilisateur}: {mdp_crypte}")
+# Fonction pour obtenir le mot de passe de l'utilisateur
+def mdputilisateur():
+    affexigencemdp()  # Afficher les exigences de sécurité
+    while True:
+        mdp = input("Choisissez un mot de passe : ")
+        if verifmdpexigence(mdp):
+            return mdp
         else:
-            print("Aucun mot de passe enregistré.")
+            print("Le mot de passe ne respecte pas les exigences de sécurité. Veuillez réessayer.")
 
+# Fonction pour hacher le mot de passe en utilisant l'algorithme SHA-256
+def hashmdp(mdp):
+    hashed_password = hashlib.sha256(mdp.encode()).hexdigest()
+    return hashed_password
+
+# Fonction pour enregistrer les mots de passe dans un fichier JSON
+def mdpsauvegarde(passwords):
+    with open('passwords.json', 'w') as file:
+        json.dump(passwords, file)
+
+# Fonction pour charger les mots de passe depuis le fichier JSON
+def chargemdp():
+    try:
+        with open('passwords.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+# Fonction pour afficher les exigences de sécurité du mot de passe
+def affexigencemdp():
+    print("Exigences de sécurité du mot de passe :")
+    print("1. Il doit contenir au moins huit caractères.")
+    print("2. Il doit contenir au moins une lettre majuscule.")
+    print("3. Il doit contenir au moins une lettre minuscule.")
+    print("4. Il doit contenir au moins un chiffre.")
+    print("5. Il doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *).")
+
+# Fonction principale
 def main():
-    gestionnaire = GestionMotDePasse()
+    # Charger les mots de passe existants depuis le fichier JSON
+    passwords = chargemdp()
 
     while True:
+        # Afficher le menu de choix
+        print("\nMenu :")
         print("1. Ajouter un nouveau mot de passe")
         print("2. Afficher les mots de passe enregistrés")
-        print("3. Quitter")
+        print("0. Quitter")
 
-        choix = input("Choisissez une option (1, 2 ou 3) : ")
+        # Obtenir le choix de l'utilisateur
+        menuchoix = input("Veuillez entrer votre choix : ")
 
-        if choix == "1":
-            utilisateur = input("Nom d'utilisateur : ")
-            gestionnaire.ajoutmdp(utilisateur)
-        
-        elif choix == "2":
-            gestionnaire.affichermotsdepasse()
+        if menuchoix == '1':
+            # Ajouter un nouveau mot de passe
+            user_password = mdputilisateur()
+            hashed_password = hashmdp(user_password)
 
-        elif choix == "3":
+            # Vérifier si le mot de passe est déjà présent dans la liste
+            if hashed_password not in passwords:
+                # Ajouter le mot de passe à la liste
+                passwords.append(hashed_password)
+                # Enregistrer la liste mise à jour dans le fichier JSON
+                mdpsauvegarde(passwords)
+                print("Mot de passe enregistré avec succès !")
+            else:
+                print("Ce mot de passe existe déjà dans la base de données. Veuillez en choisir un nouveau.")
+        elif menuchoix == '2':
+            # Afficher les mots de passe enregistrés
+            print("Mots de passe enregistrés :")
+            for hashed_pass in passwords:
+                print(hashed_pass)
+        elif menuchoix == '0':
+            # Quitter le programme si l'utilisateur choisit 0
             break
-
         else:
-            print("Option invalide. Veuillez choisir une option valide.")
+            print("Choix invalide. Veuillez réessayer.")
 
+# Exécuter la fonction principale si le script est exécuté directement
 if __name__ == "__main__":
     main()
